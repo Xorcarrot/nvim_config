472214fc2e17a8f44e0c6f79931527a5af9353a7
Patrick Pusswald <patrick.pusswald@idm-energie.at>
2026-02-04 15:16:41 +0100

    added resize mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,18 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Rsizing Commands
+map("n", "<C-right>", [[<cmd>vertical resize +5<cr>]])
+map("n", "<C-left>", [[<cmd>vertical resize -5<cr>]])
+map("n", "<C-up>", [[<cmd>horizontal resize +2<cr>]])
+map("n", "<C-down>", [[<cmd>horizontal resize -2<cr>]])
+
+-- Terminal toggeln
+map({ "n", "t" }, "<F1>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "floatTerm" }
+end, { desc = "Toggle Floating Terminal" })
+
+-- Terminal (Normal Mode)
+map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit Terminal Mode" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
